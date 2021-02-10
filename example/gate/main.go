0c40954ca3e10214fa5c4d64bc3b4a02f4c190f9
alpr777 <al.pr777@gmail.com>
2021-02-10 17:21:41 +0200

    add gate

---
+++ example/gate/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/alpr777/homekit"
+	"github.com/brutella/hc"
+	"github.com/brutella/hc/accessory"
+)
+
+const (
+	accessoryName string = "gate"
+	accessorySn   string = "ExmplGT"
+	accessoryPin  string = "11112222"
+)
+
+func main() {
+	// runtime.GOMAXPROCS(4)
+	// log.Debug.Enable()
+	acc := homekit.NewAccessoryGate(accessory.Info{Name: accessoryName, SerialNumber: accessorySn, Manufacturer: "EXAMPLE", Model: "ACC-TEST", FirmwareRevision: "1.2"})
+	transp, err := hc.NewIPTransport(hc.Config{StoragePath: "./" + acc.Info.SerialNumber.GetValue(), Pin: accessoryPin}, acc.Accessory)
+	if err != nil {
+		fmt.Println("accessory [", acc.Info.SerialNumber.GetValue(), "/", acc.Info.Name.GetValue(), "]", "error create transport:", err)
+		os.Exit(1)
+	}
+	go acc.Gate.TargetDoorState.OnValueRemoteUpdate(func(state int) {
+		fmt.Printf("acc remote update on: %T - %v \n", state, state)
+		acc.Gate.CurrentDoorState.SetValue(state)
+		fmt.Printf("acc update current position: %T - %v \n", acc.Gate.CurrentDoorState.GetValue(), acc.Gate.CurrentDoorState.GetValue())
+	})
+	fmt.Println("homekit accessory transport start [", acc.Info.SerialNumber.GetValue(), "/", acc.Info.Name.GetValue(), "]")
+	hc.OnTermination(func() { <-transp.Stop() })
+	transp.Start()
+}
