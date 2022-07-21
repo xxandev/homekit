9bc29a68658f7a0c05e4477a85dbe2a91a0006e8
xxandev <al.pr777@gmail.com>
2022-07-21 10:47:23 +0300

    upgrade example bridge

--- example/bridge/main.go
+++ example/bridge/main.go
@@ -14,11 +14,11 @@ import (
 )
 
 const (
-	BRG_NAME    string = "Bridge"
-	BRG_SN      string = "EX-Brg"
-	BRG_MODEL   string = "HAP-BRG"
-	BRG_ADDRESS string = ":11102"
+	BRG_NAME    string = "Bridge-2"
+	BRG_SN      string = "EX-Brg-2"
+	BRG_ADDRESS string = ":11103"
 	BRG_PIN     string = "12344321"
+	BRG_MODEL   string = "HAP-BRG"
 )
 
 type AccessoryInterface interface {
@@ -27,9 +27,11 @@ type AccessoryInterface interface {
 }
 
 //do not use such names func!!!
-func aadd(a AccessoryInterface) *accessory.A {
+func aadd(id uint64, a AccessoryInterface) *accessory.A {
 	a.OnExample()
-	return a.GetAccessory()
+	acc := a.GetAccessory()
+	acc.Id = id
+	return acc
 }
 
 //do not use such names func!!!
@@ -40,43 +42,44 @@ func newinfo(name, sn, model string) accessory.Info {
 func main() {
 	homekit.OnLog(false)
 	bridge := accessory.NewBridge(accessory.Info{Name: BRG_NAME, SerialNumber: BRG_SN, Model: BRG_MODEL, Manufacturer: homekit.Manufacturer, Firmware: homekit.Firmware})
+	bridge.A.Id = 1
 	llog := log.New(os.Stdout, fmt.Sprintf("[ %v / %v ] ", bridge.A.Info.SerialNumber.Value(), bridge.A.Info.Name.Value()), log.Ldate|log.Ltime|log.Lmsgprefix)
 	storage := hap.NewFsStore(fmt.Sprintf("./%s", bridge.Info.SerialNumber.Value()))
 	server, err := hap.NewServer(storage, bridge.A,
-		aadd(homekit.NewAccessoryAirPurifier(newinfo("AirPurifier", "EX-AirPurifier", "HAP-AP"), 0, 0, 100, 1)),
-		aadd(homekit.NewAccessoryDoor(newinfo("Door", "EX-dr", "HAP-"))),
-		aadd(homekit.NewAccessoryFanRS(newinfo("Fan", "EX-fsp", "HAP-"))),
-		aadd(homekit.NewAccessoryFanSwitch(newinfo("Fan", "EX-fsw", "HAP-"))),
-		aadd(homekit.NewAccessoryFan2RS(newinfo("Fan2", "EX-f2sp", "HAP-"))),
-		aadd(homekit.NewAccessoryFan2Switch(newinfo("Fan2", "EX-f2sw", "HAP-"))),
-		aadd(homekit.NewAccessoryFaucet(newinfo("Faucet", "EX-fc", "HAP-"))),
-		aadd(homekit.NewAccessoryGate(newinfo("Gate", "EX-gt", "HAP-"))),
-		aadd(homekit.NewAccessoryHumidifierDehumidifier(newinfo("HumDehum", "EX-hdm", "HAP-"))),
-		aadd(homekit.NewAccessoryIrrigation(newinfo("Irrigation", "EX-irg", "HAP-"))),
-		aadd(homekit.NewAccessoryLightbulbColored(newinfo("LightColor", "EX-lc", "HAP-"))),
-		aadd(homekit.NewAccessoryLightbulbDimmer(newinfo("LightDimm", "EX-ld", "HAP-"))),
-		aadd(homekit.NewAccessoryLightbulbSwitch(newinfo("LightSwitch", "EX-ls", "HAP-"))),
-		aadd(homekit.NewAccessoryLock(newinfo("Lock", "EX-lock", "HAP-"))),
-		aadd(homekit.NewAccessoryOutlet(newinfo("Outlet", "EX-sw", "HAP-"))),
-		aadd(homekit.NewAccessorySecuritySystem(newinfo("Alarm", "EX-scr", "HAP-"))),
-		aadd(homekit.NewAccessorySensorAirQuality(newinfo("AirQuality", "EX-saq", "HAP-"))),
-		aadd(homekit.NewAccessorySensorContact(newinfo("Contact", "EX-sct", "HAP-"))),
-		aadd(homekit.NewAccessorySensorDioxide(newinfo("Dioxide", "EX-sdx", "HAP-"))),
-		aadd(homekit.NewAccessorySensorHumidity(newinfo("Humidity", "EX-shun", "HAP-"))),
-		aadd(homekit.NewAccessorySensorLeak(newinfo("Leak", "EX-sleak", "HAP-"))),
-		aadd(homekit.NewAccessorySensorLight(newinfo("Light", "EX-slt", "HAP-"))),
-		aadd(homekit.NewAccessorySensorMonoxide(newinfo("Monoxide", "EX-smx", "HAP-"))),
-		aadd(homekit.NewAccessorySensorMotion(newinfo("Motion", "EX-smt", "HAP-"))),
-		aadd(homekit.NewAccessorySensorSmoke(newinfo("Smoke", "EX-ssm", "HAP-"))),
-		aadd(homekit.NewAccessorySensorTemperature(newinfo("Temperature", "EX-stemp", "HAP-"))),
-		aadd(homekit.NewAccessorySwitch(newinfo("Switch", "EX-sw", "HAP-"))),
-		aadd(homekit.NewAccessoryTelevision(newinfo("Television", "EX-tv", "HAP-"))),
-		aadd(homekit.NewAccessoryThermostatAutomatic(newinfo("ThermAtm", "EX-trmatm", "HAP-"))),
-		aadd(homekit.NewAccessoryThermostat(newinfo("Thermostat", "EX-trm", "HAP-"))),
-		aadd(homekit.NewAccessoryThermostat(newinfo("ThermostatHtn", "EX-trmhtn", "HAP-"), 0, 0, 1, 1)),
-		aadd(homekit.NewAccessoryThermostat(newinfo("ThermostatUc", "EX-trmhuc", "HAP-"), 3, 3, 3, 0)),
-		aadd(homekit.NewAccessoryWindow(newinfo("Window", "EX-wnd", "HAP-"))),
-		aadd(homekit.NewAccessoryWindowCovering(newinfo("WindowCovering", "EX-wndcvr", "HAP-"))),
+		aadd(2, homekit.NewAccessoryAirPurifier(newinfo("AirPurifier", "EX-AirPurifier", "HAP-AP"), 0, 0, 100, 1)),
+		aadd(3, homekit.NewAccessoryDoor(newinfo("Door", "EX-dr", "HAP-DR"))),
+		aadd(4, homekit.NewAccessoryFanRS(newinfo("Fan", "EX-fsp", "HAP-FRS"))),
+		aadd(5, homekit.NewAccessoryFanSwitch(newinfo("Fan", "EX-fsw", "HAP-FS"))),
+		aadd(6, homekit.NewAccessoryFan2RS(newinfo("Fan2", "EX-f2sp", "HAP-F2RS"))),
+		aadd(7, homekit.NewAccessoryFan2Switch(newinfo("Fan2", "EX-f2sw", "HAP-F2S"))),
+		aadd(8, homekit.NewAccessoryFaucet(newinfo("Faucet", "EX-fc", "HAP-FCT"))),
+		aadd(9, homekit.NewAccessoryGate(newinfo("Gate", "EX-gt", "HAP-DT"))),
+		aadd(10, homekit.NewAccessoryHumidifierDehumidifier(newinfo("HumDehum", "EX-hdm", "HAP-HMD"))),
+		aadd(11, homekit.NewAccessoryIrrigation(newinfo("Irrigation", "EX-irg", "HAP-IRG"))),
+		aadd(12, homekit.NewAccessoryLightbulbColored(newinfo("LightColor", "EX-lc", "HAP-LBC"))),
+		aadd(13, homekit.NewAccessoryLightbulbDimmer(newinfo("LightDimm", "EX-ld", "HAP-LBD"))),
+		aadd(14, homekit.NewAccessoryLightbulbSwitch(newinfo("LightSwitch", "EX-ls", "HAP-LBS"))),
+		aadd(15, homekit.NewAccessoryLock(newinfo("Lock", "EX-lock", "HAP-LOCK"))),
+		aadd(16, homekit.NewAccessoryOutlet(newinfo("Outlet", "EX-sw", "HAP-OTL"))),
+		aadd(17, homekit.NewAccessorySecuritySystem(newinfo("Alarm", "EX-scr", "HAP-SS"))),
+		aadd(18, homekit.NewAccessorySensorAirQuality(newinfo("AirQuality", "EX-saq", "HAP-SAQ"))),
+		aadd(19, homekit.NewAccessorySensorContact(newinfo("Contact", "EX-sct", "HAP-SCNT"))),
+		aadd(20, homekit.NewAccessorySensorDioxide(newinfo("Dioxide", "EX-sdx", "HAP-SDXD"))),
+		aadd(21, homekit.NewAccessorySensorHumidity(newinfo("Humidity", "EX-shun", "HAP-SHUM"))),
+		aadd(22, homekit.NewAccessorySensorLeak(newinfo("Leak", "EX-sleak", "HAP-SL"))),
+		aadd(23, homekit.NewAccessorySensorLight(newinfo("Light", "EX-slt", "HAP-SLG"))),
+		aadd(24, homekit.NewAccessorySensorMonoxide(newinfo("Monoxide", "EX-smx", "HAP-SMNX"))),
+		aadd(25, homekit.NewAccessorySensorMotion(newinfo("Motion", "EX-smt", "HAP-SMNT"))),
+		aadd(26, homekit.NewAccessorySensorSmoke(newinfo("Smoke", "EX-ssm", "HAP-SSMK"))),
+		aadd(27, homekit.NewAccessorySensorTemperature(newinfo("Temperature", "EX-stemp", "HAP-STEMP"))),
+		aadd(28, homekit.NewAccessorySwitch(newinfo("Switch", "EX-sw", "HAP-SW"))),
+		aadd(29, homekit.NewAccessoryTelevision(newinfo("Television", "EX-tv", "HAP-TV"))),
+		aadd(30, homekit.NewAccessoryThermostatAutomatic(newinfo("ThermAtm", "EX-trmatm", "HAP-TMA"))),
+		aadd(31, homekit.NewAccessoryThermostat(newinfo("Thermostat", "EX-trm", "HAP-TM"))),
+		aadd(32, homekit.NewAccessoryThermostat(newinfo("ThermostatHtn", "EX-trmhtn", "HAP-TM"), 0, 0, 1, 1)),
+		aadd(33, homekit.NewAccessoryThermostat(newinfo("ThermostatUc", "EX-trmhuc", "HAP-TM"), 3, 3, 3, 0)),
+		aadd(34, homekit.NewAccessoryWindow(newinfo("Window", "EX-wnd", "HAP-W"))),
+		aadd(35, homekit.NewAccessoryWindowCovering(newinfo("WindowCovering", "EX-wndcvr", "HAP-WCR"))),
 	)
 	if err != nil {
 		llog.Fatalf("error create hap server: %v\n", err)
